9d59eab4aa486fb2683908b8b4f4a9a6b51d18c8
German Burdin <germanburdin1@gmail.com>
2023-05-15 13:53:49 +0200

    destinations-model

--- src/model/destinations-model.js
+++ src/model/destinations-model.js
@@ -1,8 +1,26 @@
+import RoutePoint from './routePoint';
 import { getDestinationList } from '../mock/destination-data.js';
 
 export default class DestinationsModel {
   getDestinationsList() {
-    return getDestinationList();
+    const destinations = getDestinationList();
+    return destinations.map((destination) => {
+      const { id, name, description, pictures } = destination;
+      const routePoint = new RoutePoint(
+        id,
+        null,
+        null,
+        null,
+        {
+          name,
+          description,
+          pictures,
+        },
+        false,
+        null,
+        null
+      );
+      return routePoint;
+    });
   }
 }
-
