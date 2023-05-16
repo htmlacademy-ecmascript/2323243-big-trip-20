d9a907c9f01602af68fbc01ea33853f6361b1ce4
German Burdin <germanburdin1@gmail.com>
2023-05-16 15:33:39 +0200

    homework

--- src/model/destination-model.js
+++ src/model/destination-model.js
@@ -1,30 +1,3 @@
-// import RoutePoint from './routePoint';
-// import { getDestinationList } from '../mock/destination-data.js';
-
-// export default class DestinationsModel {
-//   getDestinationsList() {
-//     const destinations = getDestinationList();
-//     return destinations.map((destination) => {
-//       const { id, name, description, pictures } = destination;
-//       const routePoint = new RoutePoint(
-//         id,
-//         null,
-//         null,
-//         null,
-//         {
-//           name,
-//           description,
-//           pictures,
-//         },
-//         false,
-//         null,
-//         null
-//       );
-//       return routePoint;
-//     });
-//   }
-// }
-
 export class DestinationModel {
   constructor(data) {
     this.id = data.id;
--- src/model/offers-model.js
+++ src/model/offers-model.js
@@ -1,24 +1,3 @@
-// import { getOffersList } from '../mock/offers-data.js';
-// import RoutePoint from '../model/routePoint.js';
-// export default class OffersModel {
-//   getOffersList() {
-//     const offers = getOffersList();
-//     return offers.map((offer) => {
-//       const { id, title, price, type, offers: subOffers } = offer;
-//       return new RoutePoint(
-//         id,
-//         price,
-//         null,
-//         null,
-//         null,
-//         false,
-//         subOffers,
-//         type
-//       );
-//     });
-//   }
-// }
-
 export class OfferModel {
   constructor(data) {
     this.id = data.id;
