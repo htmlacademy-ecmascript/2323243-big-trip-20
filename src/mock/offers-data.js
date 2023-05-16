45af52476da4cad8738e8fe18eb6ba3163d68c5e
German Burdin <germanburdin1@gmail.com>
2023-05-16 10:38:09 +0200

    промежуточная работа

--- src/mock/offers-data.js
+++ src/mock/offers-data.js
@@ -1,78 +1,78 @@
-import { util } from '../util.js';
-import { CONST_DATA } from './const-data.js';
-import RoutePoint from '../model/routePoint.js';
+// import { util } from '../util.js';
+// import { CONST_DATA } from './const-data.js';
+// import RoutePoint from '../model/routePoint.js';
 
-const getOffer = (offerTitle) => {
-  const newOffer = {
-    id: util.getUniqId(),
-    title: offerTitle,
-    price: util.getRandomPrice()
-  };
-  return new RoutePoint(
-    newOffer.id,
-    newOffer.price,
-    null,
-    null,
-    {
-      name: newOffer.title,
-      description: '',
-      pictures: []
-    },
-    false,
-    null,
-    null
-  );
-};
+// const getOffer = (offerTitle) => {
+//   const newOffer = {
+//     id: util.getUniqId(),
+//     title: offerTitle,
+//     price: util.getRandomPrice()
+//   };
+//   return new RoutePoint(
+//     newOffer.id,
+//     newOffer.price,
+//     null,
+//     null,
+//     {
+//       name: newOffer.title,
+//       description: '',
+//       pictures: []
+//     },
+//     false,
+//     null,
+//     null
+//   );
+// };
 
-const getOffers = () => {
-  const newOffers = [];
-  for (let i = 0; i < util.getRandomCount(CONST_DATA.offers.length); i++) {
-    newOffers[i] = getOffer(CONST_DATA.offers[i]);
-  }
-  return newOffers;
-};
-
-
-const getAllOffers = () => {
-  const allOffers = [];
-  for (let i = 0; i < util.getRandomCount(CONST_DATA.countLimit); i++) {
-    allOffers[i] = {
-      type: util.getRandomArrayElement(CONST_DATA.typeOffers),
-      offers: getOffers()
-    };
-  }
-
-  return allOffers.map((offer) => {
-    const { type, offers: subOffers } = offer;
-    return new RoutePoint(
-      util.getUniqId(),
-      null,
-      null,
-      null,
-      {
-        name: '',
-        description: '',
-        pictures: []
-      },
-      false,
-      subOffers,
-      type
-    );
-  });
-};
+// const getOffers = () => {
+//   const newOffers = [];
+//   for (let i = 0; i < util.getRandomCount(CONST_DATA.offers.length); i++) {
+//     newOffers[i] = getOffer(CONST_DATA.offers[i]);
+//   }
+//   return newOffers;
+// };
 
 
-const offers = getAllOffers();
-const getOffersId = (type) => {
-  const id = [];
-  offers.forEach((offer) => {
-    if (offer.type === type) {
-      offer.offers.forEach((element) => id.push(element.id));
-    }
-  });
-  return id;
-};
+// const getAllOffers = () => {
+//   const allOffers = [];
+//   for (let i = 0; i < util.getRandomCount(CONST_DATA.countLimit); i++) {
+//     allOffers[i] = {
+//       type: util.getRandomArrayElement(CONST_DATA.typeOffers),
+//       offers: getOffers()
+//     };
+//   }
 
-const getOffersList = () => offers;
+//   return allOffers.map((offer) => {
+//     const { type, offers: subOffers } = offer;
+//     return new RoutePoint(
+//       util.getUniqId(),
+//       null,
+//       null,
+//       null,
+//       {
+//         name: '',
+//         description: '',
+//         pictures: []
+//       },
+//       false,
+//       subOffers,
+//       type
+//     );
+//   });
+// };
 
-export { getOffersId , getOffersList };
+
+// const offers = getAllOffers();
+// const getOffersId = (type) => {
+//   const id = [];
+//   offers.forEach((offer) => {
+//     if (offer.type === type) {
+//       offer.offers.forEach((element) => id.push(element.id));
+//     }
+//   });
+//   return id;
+// };
+
+// const getOffersList = () => offers;
+
+// export { getOffersId , getOffersList };
--- src/mock/point-data.js
+++ src/mock/point-data.js
@@ -1,36 +1,36 @@
-import { getRandomPeriod, util } from '../util.js';
-import { CONST_DATA } from './const-data.js';
-import { getOffersList, getOffersId } from './offers-data.js';
-import { destinationList, getDestinationId } from './destination-data.js';
-import RoutePoint from '../model/routePoint.js';
+// import { getRandomPeriod, util } from '../util.js';
+// import { CONST_DATA } from './const-data.js';
+// import { getOffersList, getOffersId } from './offers-data.js';
+// import { destinationList, getDestinationId } from './destination-data.js';
+// import RoutePoint from '../model/routePoint.js';
 
-const getNewTripPoint = () => {
-  const randomPeriod = getRandomPeriod();
-  const typePoint = util.getRandomArrayElement(getOffersList()).type;
-  const city = util.getRandomArrayElement(destinationList).name;
-  const tripPoint = {
-    id: util.getUniqId(),
-    basePrice: util.getRandomPrice(),
-    dateFrom: randomPeriod[0],
-    dateTo: randomPeriod[1],
-    destination: getDestinationId(city),
-    isFavorite: util.getRandomBooleanValue(),
-    offers: [
-      getOffersId(typePoint)
-    ],
-    type: typePoint
-  };
-  const routePoint = new RoutePoint(tripPoint);
-  return routePoint;
-};
+// const getNewTripPoint = () => {
+//   const randomPeriod = getRandomPeriod();
+//   const typePoint = util.getRandomArrayElement(getOffersList()).type;
+//   const city = util.getRandomArrayElement(destinationList).name;
+//   const tripPoint = {
+//     id: util.getUniqId(),
+//     basePrice: util.getRandomPrice(),
+//     dateFrom: randomPeriod[0],
+//     dateTo: randomPeriod[1],
+//     destination: getDestinationId(city),
+//     isFavorite: util.getRandomBooleanValue(),
+//     offers: [
+//       getOffersId(typePoint)
+//     ],
+//     type: typePoint
+//   };
+//   const routePoint = new RoutePoint(tripPoint);
+//   return routePoint;
+// };
 
-const getTripPoints = () => {
-  const tripPoints = [];
-  for (let i = 0; i < util.getRandomCount(CONST_DATA.countLimit); i++) {
-    tripPoints.push(getNewTripPoint());
-  }
-  return tripPoints;
-};
+// const getTripPoints = () => {
+//   const tripPoints = [];
+//   for (let i = 0; i < util.getRandomCount(CONST_DATA.countLimit); i++) {
+//     tripPoints.push(getNewTripPoint());
+//   }
+//   return tripPoints;
+// };
 
-export { getTripPoints, getNewTripPoint };
+// export { getTripPoints, getNewTripPoint };
 
