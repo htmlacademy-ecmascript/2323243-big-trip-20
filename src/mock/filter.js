d61073140bb0b2e8706e191011b6e82d38104b15
German <125369843+GermanBurdin1@users.noreply.github.com>
2023-06-24 17:13:37 +0200

    свои правки и комменты

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,13 +1,25 @@
 import { filter } from '../utils/filter.js';
 
 function generateFilters(points) {
-  return Object.entries(filter)
-    .map(([filterType, filterPoints]) => ({
+  return Object.entries(filter) /* [
+    [FilterType.EVERYTHING, (points) => [...points]],
+    [FilterType.FUTURE, (points) => points.filter((point) => isPointFuture(point))],
+    [FilterType.PRESENT, (points) => points.filter((point) => isPointPresent(point))],
+    [FilterType.PAST, (points) => points.filter((point) => isPointPast(point))]
+  ]
+   */
+    .map(([filterType, filterPoints]) => ({ //значения извлекаются из filter
       type: filterType,
       hasPoints: filterPoints(points).length > 0,
     }),
     );
 }
+/* [
+  { type: 'everything', hasPoints: true },
+  { type: 'future', hasPoints: true },
+  { type: 'present', hasPoints: true },
+  { type: 'past', hasPoints: false }
+] */
 
 export { generateFilters };
 
